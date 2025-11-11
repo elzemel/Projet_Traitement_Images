91c12b899f2908f9b2458436d287c912fd7a9993
elsa <elsaland91940@gmail.com>
2025-11-11 22:20:46 +0100

    a

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+
+int main() {
+    Mat img = imread("test.jpg");
+    if (img.empty()) {
+        std::cout << "Image not found!\n";
+        return -1;
+    }
+    imshow("Display", img);
+    waitKey(0);
+    return 0;
+}
+
+//g++ main.cpp -o test `pkg-config --cflags --libs opencv4` ./test
